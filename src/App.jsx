80118347d0ab52ac45e39c4f48aeb9f1d71321c4
erayerm <ermemiseray@gmail.com>
2024-01-31 16:51:46 +0300

    creating skeleton

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Greetings from "./components/Greetings"
+import Profile from "./components/Profile"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
+import "./reset.css";
+
+function App() {
+
+  return (
+    <>
+      <Greetings />
+      <Skills />
+      <Profile />
+      <Projects />
+    </>
+  )
+}
+
+export default App
