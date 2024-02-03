a0106eb27f2583c2bb7a86e1efecc9a273bc77ed
erayerm <ermemiseray@gmail.com>
2024-02-03 22:44:07 +0300

    profile section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
+import { useEffect } from "react";
 import Greetings from "./components/Greetings"
 import Profile from "./components/Profile"
 import Projects from "./components/Projects"
 import Skills from "./components/Skills"
 import "./reset.css";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "./store/actions";
 
 function App() {
-
+  const dispatch = useDispatch();
+  const darkMode = useSelector((store)=>store.darkMode);
+  useEffect(()=>{
+    dispatch(fetchData());
+  },[])
   return (
-    <>
+    <div className={darkMode ? 'dark' : ""}>
       <Greetings />
       <Skills />
       <Profile />
       <Projects />
-    </>
+    </div>
   )
 }
 
---
+++ src/components/Profile.jsx
@@ -0,0 +1,60 @@
+import {data} from "../mock/data"
+import { useSelector } from "react-redux";
+
+export default function Profile() {
+  const lang = useSelector((store) => store.lang);
+  return (
+    <>
+      <div className="w-screen dark:bg-bgDarkBlack bg-bgLightGray py-20">
+        <div className="max-w-[1100px] flex flex-col items-center my-0 mx-auto">
+          <h2 className="text-4xl font-medium tracking-[0.01em] dark:text-white ">
+            {data[lang].profile.title}
+          </h2>
+          <div className="w-full flex justify-between pt-10">
+            <div className="pl-10 pb-10 max-w-[520px] w-6/12 bg-white dark:bg-[#525252] rounded-xl shadow-[10px_10px_rgba(82,82,82,0.5)]">
+              <h3 className="text-[#ea2678] font-playfair text-2xl font-normal tracking-[0.015em] pt-10">
+                {data[lang].profile.basicInformation.title}
+              </h3>
+              <div className="flex flex-col">
+                {data[lang].profile.basicInformation.content.map(
+                  (item, index) => {
+                    return (
+                      <div
+                        className="pt-5 flex gap-1.5 text-black dark:text-white tracking-[0.01em]"
+                        key={index}
+                      >
+                        <p className="w-[32%] font-lg font-semibold">
+                          {item.first}
+                        </p>
+                        <p className="w-[50%] font-lg font-normal">
+                          {item.second}
+                        </p>
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+            </div>
+            <div className="pt-10 w-[500px]">
+              <h3 className="font-playfair text-2xl dark:text-white font-normal tracking-[0.015em]">
+                {data[lang].profile.aboutMe.title}
+              </h3>
+              <div className="w-[90%]">
+                {data[lang].profile.aboutMe.content.map((item, index) => {
+                  return (
+                    <p
+                      className="dark:text-white pt-5 text-lg font-normal tracking-[0.01em]"
+                      key={index}
+                    >
+                      {item}
+                    </p>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
