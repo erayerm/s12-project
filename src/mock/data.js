c142dcc70d362cdf1de9dc8d7bde9029af5f616d
erayerm <ermemiseray@gmail.com>
2024-02-04 01:29:22 +0300

    gitignore

---
+++ src/mock/data.js
@@ -0,0 +1,134 @@
+export const data = {
+  en: {
+    navBar: {
+      darkModeOn: "LIGHT MODE",
+      darkModeOff: "DARK MODE",
+      lang: "TÜRKÇE'YE GEÇ",
+    },
+    greetings: {
+      hiText: "Hi! 👋",
+      bigText:
+        "I’m Eray. I’m a full-stack developer. I can craft solid and scalable frontend products. Let’s meet!",
+      bottomText:
+        "Currently Freelancing for UX, UI, & Web Design Project. Invite me to join your team -> ermemiseray@gmail.com",
+    },
+    skills: {
+      title: "Skills",
+    },
+    profile: {
+      title: "Profile",
+      aboutMe: {
+        title: "About me",
+        content: [
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
+          "Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
+        ],
+      },
+      basicInformation: {
+        title: "Basic Information",
+        content: [
+          {
+            first: "Birth Date",
+            second: "09.03.2000",
+          },
+          {
+            first: "City",
+            second: "Istanbul",
+          },
+          {
+            first: "Education",
+            second: "Sakarya University, Computer Engineering",
+          },
+          {
+            first: "Preffered Role",
+            second: "Full-Stack",
+          },
+        ],
+      },
+    },
+    projects: {
+      title: "Projects",
+      project: [
+        {
+          title: "Random Jokes",
+          info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
+          tags: ["react", "vercel", "axios", "router"],
+        },
+        {
+          title: "Are you bored?",
+          info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
+          tags: ["react", "redux", "vercel", "axios", "router"],
+        },
+      ],
+      viewOnGithub: "View on Github",
+      goToApp: "Go to app ->",
+      letsWork: "Let’s work together on your next product.",
+    },
+  },
+  tr: {
+    navBar: {
+      darkModeOn: "AYDINLIK MOD",
+      darkModeOff: "KARANLIK MOD",
+      lang: "GO TO ENGLISH",
+    },
+    greetings: {
+      hiText: "Selam! 👋",
+      bigText:
+        "Ben Eray, Full-Stack Developer'ım. Sağlam ve ölçeklenebilir web sayfaları üretebilirim. Hadi Tanışalım!",
+      bottomText:
+        "UX, UI, & Web Design Projeleri için freelance çalışabilirim. Beni takımına davet et -> pratamaiosi@gmail.com",
+    },
+    skills: {
+      title: "Yetenekler",
+    },
+    profile: {
+      title: "Profil",
+      aboutMe: {
+        title: "Hakkımda",
+        content: [
+          "Tr Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
+          "Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
+        ],
+      },
+      basicInformation: {
+        title: "Bilgiler",
+        content: [
+          {
+            first: "Doğum Tarihi",
+            second: "09.03.2000",
+          },
+          {
+            first: "İkamet Şehri",
+            second: "İstanbul",
+          },
+          {
+            first: "Eğitim Durumu",
+            second: "Sakarya Üniversitesi, Bilgisayar Mühendisliği",
+          },
+          {
+            first: "Tercih Ettiği Rol",
+            second: "Full-Stack",
+          },
+        ],
+      },
+    },
+    projects: {
+      title: "Projeler",
+      project: [
+        {
+          title: "Rastgele Jokes",
+          info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
+          tags: ["react", "vercel", "axios", "router"],
+        },
+        {
+          title: "Are you sıkıldı?",
+          info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
+          tags: ["react", "redux", "vercel", "axios", "router"],
+        },
+      ],
+      viewOnGithub: "Github'da görüntüle",
+      goToApp: "App'e git ->",
+      letsWork: "Let’s work together on your next product.",
+    },
+  },
+};
