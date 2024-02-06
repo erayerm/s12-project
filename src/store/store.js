9494fc0442654d50f9ccbaedde4882aaffc9df95
erayerm <ermemiseray@gmail.com>
2024-02-07 02:25:13 +0300

    using localStorage for language

--- src/store/store.js
+++ src/store/store.js
@@ -1,49 +1,52 @@
-
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import { thunk } from "redux-thunk";
-
 
 export const GlobalActionTypes = {
   changeLanguage: "CHANGE_LANG",
   toggleDarkMode: "DARK_MODE",
-  setData: "SET_DATA"
+  setData: "SET_DATA",
 };
-const browserDefaultDark = window.matchMedia('(prefers-color-scheme: dark)');
+const browserDefaultDark = window.matchMedia("(prefers-color-scheme: dark)");
 const localDarkMode = localStorage.getItem("darkMode");
+const localLanguage = localStorage.getItem("language");
+
 const initialState = {
   lang:
-    (navigator.language || navigator.userLanguage) === "tr-TR" ||
-    (navigator.language || navigator.userLanguage) === "tr"
-      ? "tr"
-      : "en",
+    localLanguage === null
+      ? (navigator.language || navigator.userLanguage) === "tr-TR" ||
+        (navigator.language || navigator.userLanguage) === "tr"
+        ? "tr"
+        : "en"
+      : localLanguage,
   darkMode:
-    localDarkMode === null ? browserDefaultDark.matches : JSON.parse(localDarkMode),
+    localDarkMode === null
+      ? browserDefaultDark.matches
+      : JSON.parse(localDarkMode),
   data: [],
 };
 
-
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-      case GlobalActionTypes.changeLanguage:
-        return {
-          ...state,
-          lang: state.lang === "en" ? "tr" : "en"
-        };
-      case GlobalActionTypes.toggleDarkMode:
-        localStorage.setItem("darkMode", !state.darkMode);
-        return {
-          ...state,
-          darkMode: !state.darkMode,
-        };
-      case GlobalActionTypes.setData:
-        console.log(action.payload)
-        return {
-          ...state,
-          data: {...action.payload},
-        };
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case GlobalActionTypes.changeLanguage:
+      localStorage.setItem("language", state.lang === "en" ? "tr" : "en");
+      return {
+        ...state,
+        lang: state.lang === "en" ? "tr" : "en",
+      };
+    case GlobalActionTypes.toggleDarkMode:
+      localStorage.setItem("darkMode", !state.darkMode);
+      return {
+        ...state,
+        darkMode: !state.darkMode,
+      };
+    case GlobalActionTypes.setData:
+      return {
+        ...state,
+        data: { ...action.payload },
+      };
+    default:
+      return state;
+  }
 };
 
 export const store = createStore(reducer, applyMiddleware(thunk));
